58ae267949e68861ab5100b07bb67be30ab59292
nisjain12 <150013749+nisjain12@users.noreply.github.com>
2023-11-06 15:19:57 +0530

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("Mayank");
+		person.setFirstName("Mayankyy");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
